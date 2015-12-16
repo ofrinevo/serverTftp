75bc55840ca0617cdc5f9ee7f293211ef25a2727
ofrinevo <ofrine@gmail.com>
2015-12-16 15:08:22 +0200

    init

---
+++ tftp/tftp/tftp-server.c
@@ -0,0 +1,20 @@
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <strings.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <fcntl.h>
+#include <err.h>
+#include <time.h>
+
+int main() {
+	return 0;
+}
