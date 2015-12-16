4c248f05423c063410828c5ee2c1c1fe081ac300
ofrinevo <ofrine@gmail.com>
2015-12-16 16:17:28 +0200

    hello

--- tftp/tftp/tftp-server.c
+++ tftp/tftp/tftp-server.c
@@ -14,6 +14,37 @@
 #include <fcntl.h>
 #include <err.h>
 #include <time.h>
+#define SIZE 512
+
+typedef struct readSize {
+	short blockNumber;
+	int sizeRead;
+} readSize;
+
+int sendData(FILE* fp,short blockNumber,int sockfd,const struct sockaddr *dest_adrr,socklen_t addrLen){
+	if (fseek(fp, blockNumber*SIZE, SEEK_SET)) {
+		perror("fseek");
+		return -1;
+	}
+	char buf[SIZE+4];
+	short opcode = 3;
+	if (sprintf(buf, "%hd%hd", opcode, blockNumber)) {
+		perror("sprintf");
+		return -1;
+	}
+		
+	int sizeRead=fread(buf, SIZE, 1, fp);
+	if (ferror(fp)) {
+		perror("fread");
+		return -1;
+	}
+	sendto(sockfd, &buf, SIZE + 4, 0, dest_adrr, addrLen);
+	return sizeRead;
+}
+
+readSize recvData(FILE* fp,char* buf, int sockfd, const struct sockaddr *dest_adrr, socklen_t addrLen) {
+
+}
 
 int main() {
 	return 0;
